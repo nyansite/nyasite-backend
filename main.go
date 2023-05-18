a18ac6b27d7d29a32d4099a6f88f581078164a97
tksmly <tksmly@gmail.com>
2023-05-18 19:50:29 +0800

    挂几个todo

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/memstore"
 	"github.com/gin-gonic/gin"
-
 	// "gorm.io/driver/sqlite"
 	// "gorm.io/gorm"
 
@@ -19,13 +18,17 @@ func main() {
 	store := memstore.NewStore([]byte("just_secret"))
 	store.Options(sessions.Options{Secure: true, HttpOnly: true})
 	r.Use(sessions.Sessions("session_id", store))
+	// TODO csrf防护,需要前端支持
 
 	// db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	// if err != nil {
 	// 	panic("我数据库呢???我那么大一个数据库呢???还我数据库!!!")
 	// }
 
-	r.GET("/api/user_status", get_user_status)
+	r.GET("/api/user_status", get_self_user_status)
+	r.GET("/api/user_status/:id", get_self_user_status)
+
+
 	r.POST("/api/register", post_register)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
@@ -34,7 +37,7 @@ func main() {
 
 
 
-func get_user_status(c *gin.Context) {
+func get_self_user_status(c *gin.Context) {
 	
 	session := sessions.Default(c)
 	userid := 0 //userid为零表示错误
@@ -48,6 +51,10 @@ func get_user_status(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"userid": userid,
 	})
+}
+
+func get_user_status(c *gin.Context)  {
+	// TODO 根据id获取
 }
 
 func login(c *gin.Context) {
