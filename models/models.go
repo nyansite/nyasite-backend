73992524bc73ddfa6c8e1e4b5c9802f33d0a861d
tksmly <tksmly@gmail.com>
2023-05-20 18:22:42 +0800

    w

--- models/models.go
+++ models/models.go
@@ -6,57 +6,59 @@ import (
 
 type User struct {
 	gorm.Model        //用模型本身的id
-	Name       string `gorm:"unique"`
-	Passwd     string //TODO 记得加盐
-	Email      string `gorm:"unique"`
+	Name	string `gorm:"unique"`
+	Passwd	[]byte //TODO 记得加盐
+	Email	string `gorm:"unique"`
+	level	uint	//4位权限4位等级,所以满级15
 }
 
 //视频部分
 
-type VideoRequireReview struct {
+type VideoPreviewRequire struct {
 	gorm.Model
-	CoverFile    string `json:"cover`
-	VideoFile    string `json:"videofile"`
-	UpId_p       string `json:"up_p"`
-	Title        string `json:"title"`
-	Pass         uint   `json:"pass"`
-	Introduction string `json:"introduction"`
+	CoverFile    string
+	VideoFile    string
+	Title        string
+	Pass         uint
+	Introduction string
 }
 
+//说好的不用ipfs呢??????
 type Video struct {
 	gorm.Model
-	VideoIpfsSite string `json:"VideoIpfsSite"`
-	CoverFile     string `json:"coverfile"`
-	UpId_p        string `json:"up_p"`
-	Title         string `json:"title"`
-	Introduction  string `json:"introduction"`
-	Views         uint   `json:"views"`
+	VideoLink 	string
+	CoverLink   string		//封面也用磁力链接
+	Title       string
+	Profile  	string		//芝士简介
+	Comment		[]Comment	//评论	
+	// Views         	uint	//这是什么
 }
 
 //论坛部分
+//TDOD 这块要重构
+// type MainPost struct {
+// 	gorm.Model
+// 	Title       string `json:"title"`
+// 	User_p      string `json:"user_p"` //发起人
+// 	Views       uint   `json:"views"`
+// 	Video_p     string `json:"video_p"` //如果帖子是视频的评论区存储视频id，如果不是存储"independent"
+// 	ContentShow string `json:"contentshow"`
+// }
 
-type MainPost struct {
-	gorm.Model
-	Title       string `json:"title"`
-	User_p      string `json:"user_p"` //发起人
-	Views       uint   `json:"views"`
-	Video_p     string `json:"video_p"` //如果帖子是视频的评论区存储视频id，如果不是存储"independent"
-	ContentShow string `json:"contentshow"`
-}
-
-type UnitPost struct {
-	gorm.Model
-	MainPost_p string `json:"mainpost_p"`
-	Content    string `json:"content"` //以md形式储存
-	User_p     string `json:"user_p"`
-	Level      uint   `json:"level"` //楼层
-}
+// type UnitPost struct {
+// 	gorm.Model
+// 	MainPost_p string `json:"mainpost_p"`
+// 	Content    string `json:"content"` //以md形式储存
+// 	User_p     string `json:"user_p"`
+// 	Level      uint   `json:"level"` //楼层
+// }
 
 // 一个mainpost下面挂着unitpost
 
+
 type Comment struct {
 	gorm.Model
-	Post_p  string `json:"post_p"`
-	Content string `json:"content"`
-	User_p  string `json:"user_p"`
+	Text	string
+	Type 	uint8	//0为字符串,1为markdown
+	Author  uint
 }
--- upload/upload.go
+++ upload/upload.go
@@ -1,49 +1,49 @@
 package upload
 
-import (
-	"cute_site/models"
-	"fmt"
-	"net/http"
+// import (
+// 	"cute_site/models"
+// 	"fmt"
+// 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"gorm.io/gorm"
+// )
 
-func Upload(db *gorm.DB, c *gin.Context) {
-	//获取标题和简介
-	up_p := c.PostForm("up_p")
-	title := c.PostForm("title")
-	introduction := c.PostForm("introduction")
-	fmt.Println(up_p)
-	//新建一个待检视频的记录
-	var videoUpload models.VideoRequireReview
-	db.AutoMigrate(&models.VideoRequireReview{})
-	db.Create(&models.VideoRequireReview{Title: title, Introduction: introduction, UpId_p: up_p, Pass: 0})
-	db.Last(&videoUpload)
-	file, err := c.FormFile("file")
-	//将上传路径定义为/media/vntc/+待检视频的id
-	id := fmt.Sprintf("%d", videoUpload.ID)
-	dst := "./media/vntc/" + id + "/" + file.Filename
-	videoUpload.VideoFile = dst
-	//一个规范的纠错部分
-	if err != nil {
-		c.String(http.StatusBadRequest, "get form err: %s", err.Error())
-		return
-	}
-	if err := c.SaveUploadedFile(file, dst); err != nil {
-		c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
-		return
-	}
-	cover, err := c.FormFile("cover")
-	dst = "./media/vntc/" + id + "/" + cover.Filename
-	videoUpload.CoverFile = dst
-	if err != nil {
-		c.String(http.StatusBadRequest, "get form err: %s", err.Error())
-		return
-	}
-	if err := c.SaveUploadedFile(cover, dst); err != nil {
-		c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
-		return
-	}
-	db.Save(&videoUpload)
-}
+// func Upload(db *gorm.DB, c *gin.Context) {
+// 	//获取标题和简介
+// 	up_p := c.PostForm("up_p")
+// 	title := c.PostForm("title")
+// 	introduction := c.PostForm("introduction")
+// 	fmt.Println(up_p)
+// 	//新建一个待检视频的记录
+// 	var videoUpload models.VideoRequireReview
+// 	db.AutoMigrate(&models.VideoRequireReview{})
+// 	db.Create(&models.VideoRequireReview{Title: title, Introduction: introduction, UpId_p: up_p, Pass: 0})
+// 	db.Last(&videoUpload)
+// 	file, err := c.FormFile("file")
+// 	//将上传路径定义为/media/vntc/+待检视频的id
+// 	id := fmt.Sprintf("%d", videoUpload.ID)
+// 	dst := "./media/vntc/" + id + "/" + file.Filename
+// 	videoUpload.VideoFile = dst
+// 	//一个规范的纠错部分
+// 	if err != nil {
+// 		c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+// 		return
+// 	}
+// 	if err := c.SaveUploadedFile(file, dst); err != nil {
+// 		c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+// 		return
+// 	}
+// 	cover, err := c.FormFile("cover")
+// 	dst = "./media/vntc/" + id + "/" + cover.Filename
+// 	videoUpload.CoverFile = dst
+// 	if err != nil {
+// 		c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+// 		return
+// 	}
+// 	if err := c.SaveUploadedFile(cover, dst); err != nil {
+// 		c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+// 		return
+// 	}
+// 	db.Save(&videoUpload)
+// }
