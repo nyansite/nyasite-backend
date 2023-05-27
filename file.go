7ba947eb157399e48656742a0a842072f6b30192
tksmly <tksmly@gmail.com>
2023-05-27 15:52:06 +0800

    add file 登录验证

--- file.go
+++ file.go
@@ -2,29 +2,31 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	shell "github.com/ipfs/go-ipfs-api"
+	"net/http"
 )
 
-// 从ipfs获取文件
-func GetFile(c *gin.Context) {
-
-}
+// 从ipfs获取文件请使用ipfs网关
 
 func AddFile(c *gin.Context) {
 	//TODO 权限
 
-	// files.NewBytesFile()
-	
-	f, _, err := c.Request.FormFile("file")    
+	session := sessions.Default(c)
+	if session.Get("is_login") != true {
+		c.AbortWithStatus(http.StatusUnauthorized) //返回401
+		return
+	}
+
+	f, _, err := c.Request.FormFile("file")
 	defer f.Close()
 	if err != nil {
 		c.AbortWithStatus(http.StatusBadRequest) //400
 		return
 	}
 
-	sh := shell.NewLocalShell()	//需要挂着ipfs daemon
+	sh := shell.NewLocalShell() //需要挂着ipfs daemon
 	sh.Add(f)
 	fmt.Println()
 }
