4c59eb4e3d3cb914a01276b3cd57c87932895eba
flyingsnoopy <lijiaqi1835@outlook.com>
2023-12-11 22:07:44 +0800

    use bucket sort

--- search.go
+++ search.go
@@ -42,6 +42,7 @@ func SearchVideos(c *gin.Context) {
 
 		}
 	}
+	//bucket sort
 	for i := range vids.Iter() {
 		if vidsCount[i] == nTags { //如果视频出现在每一种tag中，vidsCount对应项与tag种类总数相等
 			db.ID(i).Get(&video)
