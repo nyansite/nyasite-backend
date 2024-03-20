1256b0834a046a87ca1bdf934cc6502c0d39c432
tksmly <tksmly@gmail.com>
2024-03-20 14:06:47 +0800

    upload img(施工中)

--- image.go
+++ image.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/gin-gonic/gin"
+	_ "golang.org/x/image/webp"
 	"image"
 	"log"
+	"mime/multipart"
 	"net/http"
-
-	_ "golang.org/x/image/webp"
-
-	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func PostImg(ctx *gin.Context) {
-	file, err := ctx.FormFile("img")//这里已经读取文件了,文件过大会造成严重的性能问题
+	file, err := ctx.FormFile("img") //这里已经读取文件了,文件过大会造成严重的性能问题
 	if err != nil {
+		log.Println(err)
 		ctx.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
@@ -23,19 +25,30 @@ func PostImg(ctx *gin.Context) {
 		return
 	}
 	_, format, err := image.DecodeConfig(f)
-	if err != nil {
-		if err == image.ErrFormat { //只能识别加载Decode的类型
-			fmt.Println(format)
-		} else {
-			panic(err)
-		}
-	}
-	if format != "webp" { //不加载其他格式的话这个只能是webp
-		ctx.AbortWithStatus(http.StatusUnsupportedMediaType)//415 不支持的媒体格式
+	if err != nil { //只能识别加载Decode的类型
+		log.Println(err)
+		ctx.AbortWithStatus(http.StatusUnsupportedMediaType) //415 不支持的媒体格式
 		return
 	}
-
-	log.Println(format)
-	// os.Create()
+	if format != "webp" { //不加载其他格式的话这个只能是webp
+		ctx.AbortWithStatus(http.StatusUnsupportedMediaType) //415 不支持的媒体格式
+		return
+	}
+	SaveImg(f)
 	//TODO saveimg
+}
+
+func SaveImg(file multipart.File) error {
+	var ff []byte
+	file.Read(ff)
+	hash := sha256.Sum256(ff)
+	_, err := os.OpenFile("img/"+hex.EncodeToString(hash[:])+".webp", os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+	if err != nil {
+		if os.IsExist(err){
+			log.Println(err)
+			return err
+		}
+		log.Panic(err)
+	}
+	return nil
 }
